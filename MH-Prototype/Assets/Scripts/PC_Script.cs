3bd128b53fc593d8e67f14d2b4320a0f4c50f90d
rifhall <riley@kjhalloran.com>
2021-09-23 13:28:16 -0400

    Start of movement (broken)
    Movement almost works but i need to move the player movement

---
+++ MH-Prototype/Assets/Scripts/PC_Script.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PC_Script : MonoBehaviour
+{
+
+    private PlayerInputActions playerInput;
+
+    private Rigidbody2D rb;
+
+    [SerializedField] private float speed = 10f;
+
+    void Awake()
+    {
+        playerInput = new PlayerInputActions();
+        ReadOnlyCollectionBase = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        playerInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerInput.Disable();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 moveInput = playerInput.Movement.Move.ReadValue<Vector2>();
+        rb.velocity = moveInput * speed;
+    }
+}
