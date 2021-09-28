45ba2de72d2bc9853c76d155fd8d18546900a0de
rifhall <riley@kjhalloran.com>
2021-09-28 02:49:48 -0400

    Rudimentary Grappling was added
    A buggy broken version of grappling was added along with the other characters and push/pull

--- MH-Prototype/Assets/Inputs/PlayerInputActions.cs
+++ MH-Prototype/Assets/Inputs/PlayerInputActions.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/PlayerInputActions.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/Inputs/PlayerInputActions.inputactions'
 
 using System;
 using System.Collections;
@@ -23,6 +23,22 @@ public class @PlayerInputActions : IInputActionCollection, IDisposable
                     ""type"": ""PassThrough"",
                     ""id"": ""1b479800-3eff-4670-b01d-2300e6e25039"",
                     ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Pull"",
+                    ""type"": ""Button"",
+                    ""id"": ""9e52f995-caa9-491a-ad4c-bc8548ee4d08"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Stop"",
+                    ""type"": ""Button"",
+                    ""id"": ""cee41146-958d-492a-9dc0-68d3dbfc14ee"",
+                    ""expectedControlType"": ""Button"",
                     ""processors"": """",
                     ""interactions"": """"
                 }
@@ -82,6 +98,28 @@ public class @PlayerInputActions : IInputActionCollection, IDisposable
                     ""action"": ""Move"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""78257716-31c3-47ea-b83f-470e202e9724"",
+                    ""path"": ""<Keyboard>/e"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Pull"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""46fb75c4-6235-4e0d-a99e-b025ec236d38"",
+                    ""path"": ""<Keyboard>/q"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Stop"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         },
@@ -117,7 +155,7 @@ public class @PlayerInputActions : IInputActionCollection, IDisposable
             ""id"": ""00f66c0b-2e55-44e3-80da-339f937a2738"",
             ""actions"": [
                 {
-                    ""name"": ""New action"",
+                    ""name"": ""Fire"",
                     ""type"": ""Button"",
                     ""id"": ""01b9b678-5217-4a75-a7b3-582e39cd0835"",
                     ""expectedControlType"": ""Button"",
@@ -129,11 +167,38 @@ public class @PlayerInputActions : IInputActionCollection, IDisposable
                 {
                     ""name"": """",
                     ""id"": ""06ba7bea-5165-45dc-a884-271be177e377"",
-                    ""path"": """",
+                    ""path"": ""<Keyboard>/f"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": """",
-                    ""action"": ""New action"",
+                    ""action"": ""Fire"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        },
+        {
+            ""name"": ""Push"",
+            ""id"": ""68e08875-df00-404a-935f-60a9b46b787b"",
+            ""actions"": [
+                {
+                    ""name"": ""Push"",
+                    ""type"": ""Button"",
+                    ""id"": ""eae088ca-4a07-4824-8691-e6965d516f0a"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""d11df718-7f45-4526-b586-9bd9e87f8919"",
+                    ""path"": ""<Keyboard>/r"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Push"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 }
@@ -145,12 +210,17 @@ public class @PlayerInputActions : IInputActionCollection, IDisposable
         // Movement
         m_Movement = asset.FindActionMap("Movement", throwIfNotFound: true);
         m_Movement_Move = m_Movement.FindAction("Move", throwIfNotFound: true);
+        m_Movement_Pull = m_Movement.FindAction("Pull", throwIfNotFound: true);
+        m_Movement_Stop = m_Movement.FindAction("Stop", throwIfNotFound: true);
         // Selection
         m_Selection = asset.FindActionMap("Selection", throwIfNotFound: true);
         m_Selection_Newaction = m_Selection.FindAction("New action", throwIfNotFound: true);
         // Fire
         m_Fire = asset.FindActionMap("Fire", throwIfNotFound: true);
-        m_Fire_Newaction = m_Fire.FindAction("New action", throwIfNotFound: true);
+        m_Fire_Fire = m_Fire.FindAction("Fire", throwIfNotFound: true);
+        // Push
+        m_Push = asset.FindActionMap("Push", throwIfNotFound: true);
+        m_Push_Push = m_Push.FindAction("Push", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -201,11 +271,15 @@ public class @PlayerInputActions : IInputActionCollection, IDisposable
     private readonly InputActionMap m_Movement;
     private IMovementActions m_MovementActionsCallbackInterface;
     private readonly InputAction m_Movement_Move;
+    private readonly InputAction m_Movement_Pull;
+    private readonly InputAction m_Movement_Stop;
     public struct MovementActions
     {
         private @PlayerInputActions m_Wrapper;
         public MovementActions(@PlayerInputActions wrapper) { m_Wrapper = wrapper; }
         public InputAction @Move => m_Wrapper.m_Movement_Move;
+        public InputAction @Pull => m_Wrapper.m_Movement_Pull;
+        public InputAction @Stop => m_Wrapper.m_Movement_Stop;
         public InputActionMap Get() { return m_Wrapper.m_Movement; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -218,6 +292,12 @@ public class @PlayerInputActions : IInputActionCollection, IDisposable
                 @Move.started -= m_Wrapper.m_MovementActionsCallbackInterface.OnMove;
                 @Move.performed -= m_Wrapper.m_MovementActionsCallbackInterface.OnMove;
                 @Move.canceled -= m_Wrapper.m_MovementActionsCallbackInterface.OnMove;
+                @Pull.started -= m_Wrapper.m_MovementActionsCallbackInterface.OnPull;
+                @Pull.performed -= m_Wrapper.m_MovementActionsCallbackInterface.OnPull;
+                @Pull.canceled -= m_Wrapper.m_MovementActionsCallbackInterface.OnPull;
+                @Stop.started -= m_Wrapper.m_MovementActionsCallbackInterface.OnStop;
+                @Stop.performed -= m_Wrapper.m_MovementActionsCallbackInterface.OnStop;
+                @Stop.canceled -= m_Wrapper.m_MovementActionsCallbackInterface.OnStop;
             }
             m_Wrapper.m_MovementActionsCallbackInterface = instance;
             if (instance != null)
@@ -225,6 +305,12 @@ public class @PlayerInputActions : IInputActionCollection, IDisposable
                 @Move.started += instance.OnMove;
                 @Move.performed += instance.OnMove;
                 @Move.canceled += instance.OnMove;
+                @Pull.started += instance.OnPull;
+                @Pull.performed += instance.OnPull;
+                @Pull.canceled += instance.OnPull;
+                @Stop.started += instance.OnStop;
+                @Stop.performed += instance.OnStop;
+                @Stop.canceled += instance.OnStop;
             }
         }
     }
@@ -266,12 +352,12 @@ public class @PlayerInputActions : IInputActionCollection, IDisposable
     // Fire
     private readonly InputActionMap m_Fire;
     private IFireActions m_FireActionsCallbackInterface;
-    private readonly InputAction m_Fire_Newaction;
+    private readonly InputAction m_Fire_Fire;
     public struct FireActions
     {
         private @PlayerInputActions m_Wrapper;
         public FireActions(@PlayerInputActions wrapper) { m_Wrapper = wrapper; }
-        public InputAction @Newaction => m_Wrapper.m_Fire_Newaction;
+        public InputAction @Fire => m_Wrapper.m_Fire_Fire;
         public InputActionMap Get() { return m_Wrapper.m_Fire; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -281,23 +367,58 @@ public class @PlayerInputActions : IInputActionCollection, IDisposable
         {
             if (m_Wrapper.m_FireActionsCallbackInterface != null)
             {
-                @Newaction.started -= m_Wrapper.m_FireActionsCallbackInterface.OnNewaction;
-                @Newaction.performed -= m_Wrapper.m_FireActionsCallbackInterface.OnNewaction;
-                @Newaction.canceled -= m_Wrapper.m_FireActionsCallbackInterface.OnNewaction;
+                @Fire.started -= m_Wrapper.m_FireActionsCallbackInterface.OnFire;
+                @Fire.performed -= m_Wrapper.m_FireActionsCallbackInterface.OnFire;
+                @Fire.canceled -= m_Wrapper.m_FireActionsCallbackInterface.OnFire;
             }
             m_Wrapper.m_FireActionsCallbackInterface = instance;
             if (instance != null)
             {
-                @Newaction.started += instance.OnNewaction;
-                @Newaction.performed += instance.OnNewaction;
-                @Newaction.canceled += instance.OnNewaction;
+                @Fire.started += instance.OnFire;
+                @Fire.performed += instance.OnFire;
+                @Fire.canceled += instance.OnFire;
             }
         }
     }
     public FireActions @Fire => new FireActions(this);
+
+    // Push
+    private readonly InputActionMap m_Push;
+    private IPushActions m_PushActionsCallbackInterface;
+    private readonly InputAction m_Push_Push;
+    public struct PushActions
+    {
+        private @PlayerInputActions m_Wrapper;
+        public PushActions(@PlayerInputActions wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Push => m_Wrapper.m_Push_Push;
+        public InputActionMap Get() { return m_Wrapper.m_Push; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PushActions set) { return set.Get(); }
+        public void SetCallbacks(IPushActions instance)
+        {
+            if (m_Wrapper.m_PushActionsCallbackInterface != null)
+            {
+                @Push.started -= m_Wrapper.m_PushActionsCallbackInterface.OnPush;
+                @Push.performed -= m_Wrapper.m_PushActionsCallbackInterface.OnPush;
+                @Push.canceled -= m_Wrapper.m_PushActionsCallbackInterface.OnPush;
+            }
+            m_Wrapper.m_PushActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Push.started += instance.OnPush;
+                @Push.performed += instance.OnPush;
+                @Push.canceled += instance.OnPush;
+            }
+        }
+    }
+    public PushActions @Push => new PushActions(this);
     public interface IMovementActions
     {
         void OnMove(InputAction.CallbackContext context);
+        void OnPull(InputAction.CallbackContext context);
+        void OnStop(InputAction.CallbackContext context);
     }
     public interface ISelectionActions
     {
@@ -305,6 +426,10 @@ public class @PlayerInputActions : IInputActionCollection, IDisposable
     }
     public interface IFireActions
     {
-        void OnNewaction(InputAction.CallbackContext context);
+        void OnFire(InputAction.CallbackContext context);
+    }
+    public interface IPushActions
+    {
+        void OnPush(InputAction.CallbackContext context);
     }
 }
--- MH-Prototype/Assets/Scripts/PC_Script.cs
+++ MH-Prototype/Assets/Scripts/PC_Script.cs
@@ -1,24 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PC_Script : MonoBehaviour
 {
     private PlayerInputActions playerInput;
-
     private Rigidbody2D rb;
-
+    public LineRenderer line;
+    public GameObject ally;
+    public bool bound = false;
+    public bool push = false;
     [SerializeField] private float speed = 10f;
 
     void Awake()
     {
+        print("first");
         playerInput = new PlayerInputActions();
         rb = GetComponent<Rigidbody2D>();
+    }
+
+    void start()
+    {
+        line.SetWidth(0.05F, 0.05F);
+        line.SetVertexCount(2);
     }
 
     private void OnEnable()
     {
         playerInput.Enable();
+        playerInput.Fire.Fire.performed += DoFire;
+        playerInput.Fire.Fire.Enable();
+
+        playerInput.Push.Push.performed += DoPush;
+        playerInput.Push.Push.Enable();
     }
 
     private void OnDisable()
@@ -31,5 +46,53 @@ public class PC_Script : MonoBehaviour
     {
         Vector2 moveInput = playerInput.Movement.Move.ReadValue<Vector2>();
         rb.velocity = moveInput * speed;
+
+        if (bound == true && push == false)
+        {
+            line.SetPosition(0, transform.position);
+            line.SetPosition(1, ally.transform.position);
+            print("here");
+            float step = 5f * Time.deltaTime;
+            ally.transform.position = Vector3.MoveTowards(ally.transform.position, transform.position, step);
+        }
+        if (bound == false && push == true)
+        {
+            line.SetPosition(0, transform.position);
+            line.SetPosition(1, ally.transform.position);
+            print("here");
+            float step = 5f * Time.deltaTime;
+            ally.transform.position = Vector3.MoveTowards(ally.transform.position, 
+                new Vector3(-100 * transform.position.x, -100 * transform.position.y, transform.position.z), step);
+        }
+    }
+
+    private void DoFire(InputAction.CallbackContext obj)
+    {
+        if (bound == false)
+        {
+            bound = true;
+            print("dome");
+        }
+        else if (bound == true)
+        {
+            print("Happened?");
+            line.SetPosition(0, new Vector2(0,0));
+            line.SetPosition(1, new Vector2(0, 0));
+            bound = false;
+        }
+    }
+
+    private void DoPush(InputAction.CallbackContext obj)
+    {
+        if (push == false)
+        {
+            push = true;
+        }
+        else if (push == true)
+        {
+            push = false;
+            line.SetPosition(0, new Vector2(0, 0));
+            line.SetPosition(1, new Vector2(0, 0));
+        }
     }
 }
