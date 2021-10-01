392b73fde7ae715e4036fb8e4978aa3d1c777bee
rifhall <riley@kjhalloran.com>
2021-10-01 15:42:28 -0400

    finished changes for the prototype, no projectiles but health turned on
    for your entertainment

---
+++ MH-Prototype/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public int health = 10;
+    public int lifeLoss = 4;
+
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "Ally")
+        {
+            health -= 4;
+        }
+    }
+}
--- MH-Prototype/Assets/Scripts/PC_Script.cs
+++ MH-Prototype/Assets/Scripts/PC_Script.cs
@@ -9,15 +9,27 @@ public class PC_Script : MonoBehaviour
     private Rigidbody2D rb;
     public LineRenderer line;
     public GameObject ally;
+<<<<<<< Updated upstream
+=======
+    public Rigidbody2D allyRB;
+
+    //bools for help
+>>>>>>> Stashed changes
     public bool bound = false;
     public bool push = false;
     [SerializeField] private float speed = 10f;
+<<<<<<< Updated upstream
+=======
+    [SerializeField] float pullSpeed = 7f;
+    [SerializeField] Vector2 pullVelocity = new Vector2(7f, 7f);
+>>>>>>> Stashed changes
 
     void Awake()
     {
         print("first");
         playerInput = new PlayerInputActions();
         rb = GetComponent<Rigidbody2D>();
+        allyRB = ally.GetComponent<Rigidbody2D>();
     }
 
     void start()
@@ -29,6 +41,7 @@ public class PC_Script : MonoBehaviour
     private void OnEnable()
     {
         playerInput.Enable();
+
         playerInput.Fire.Fire.performed += DoFire;
         playerInput.Fire.Fire.Enable();
 
@@ -36,10 +49,7 @@ public class PC_Script : MonoBehaviour
         playerInput.Push.Push.Enable();
     }
 
-    private void OnDisable()
-    {
-        playerInput.Disable();
-    }
+    private void OnDisable(){ playerInput.Disable(); }
 
     // Update is called once per frame
     void FixedUpdate()
@@ -51,18 +61,46 @@ public class PC_Script : MonoBehaviour
         {
             line.SetPosition(0, transform.position);
             line.SetPosition(1, ally.transform.position);
+<<<<<<< Updated upstream
             print("here");
             float step = 5f * Time.deltaTime;
             ally.transform.position = Vector3.MoveTowards(ally.transform.position, transform.position, step);
+=======
+
+            float step = pullSpeed * Time.deltaTime;
+            
+            //Get the difference then move it
+            Vector2 difference = (Vector2)transform.position - (Vector2)ally.transform.position;
+
+            allyRB.MovePosition((Vector2)ally.transform.position + difference * step);
+
+            //stop movement if too close
+            if((Mathf.Abs(ally.transform.position.x - transform.position.x) < 1) &&
+                (Mathf.Abs(ally.transform.position.y - transform.position.y) < 1))
+            { 
+                bust = true;
+
+                line.SetPosition(0, new Vector2(0, 0));
+                line.SetPosition(1, new Vector2(0, 0));
+            }
+>>>>>>> Stashed changes
         }
         if (bound == false && push == true)
         {
             line.SetPosition(0, transform.position);
             line.SetPosition(1, ally.transform.position);
+<<<<<<< Updated upstream
             print("here");
             float step = 5f * Time.deltaTime;
             ally.transform.position = Vector3.MoveTowards(ally.transform.position, 
                 new Vector3(-100 * transform.position.x, -100 * transform.position.y, transform.position.z), step);
+=======
+
+            float step = pullSpeed * Time.deltaTime;
+
+            Vector2 difference = (Vector2)ally.transform.position - (Vector2)transform.position;
+            allyRB.MovePosition((Vector2)ally.transform.position + difference * step);
+>>>>>>> Stashed changes
         }
     }
 
@@ -79,6 +117,11 @@ public class PC_Script : MonoBehaviour
             line.SetPosition(0, new Vector2(0,0));
             line.SetPosition(1, new Vector2(0, 0));
             bound = false;
+<<<<<<< Updated upstream
+=======
+            bust = false;
+            allyRB.velocity = new Vector2(0f, 0f);
+>>>>>>> Stashed changes
         }
     }
 
@@ -93,6 +136,7 @@ public class PC_Script : MonoBehaviour
             push = false;
             line.SetPosition(0, new Vector2(0, 0));
             line.SetPosition(1, new Vector2(0, 0));
+            allyRB.velocity = new Vector2(0f, 0f);
         }
     }
 }
