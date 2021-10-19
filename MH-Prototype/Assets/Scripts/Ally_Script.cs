bc4b5b6e61a3ab9d55e9d3e9ed9ab4d3693fba0a
rifhall <riley@kjhalloran.com>
2021-10-19 13:47:13 -0400

    clarity changes to the script, fixed the player blocking projectiles
    minor changes

--- MH-Prototype/Assets/Scripts/Ally_Script.cs
+++ MH-Prototype/Assets/Scripts/Ally_Script.cs
@@ -5,12 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class Ally_Script : MonoBehaviour
 {
+    //internal variables
     public int health = 5;
     public int damage = 1;
+
+    //variable for health bar
     public HealthBar_Script healthBar;
 
     void Start()
     {
+        //set the healthbar up
         healthBar.SetMaxHealth(health);
     }
 
@@ -18,14 +22,15 @@ public class Ally_Script : MonoBehaviour
     {
         if (health <= 0) 
         { 
+            //destroy the object and end the game if dead
             Destroy(gameObject);
             SceneManager.LoadScene(1);
-
         }
     }
 
     private void OnCollisionEnter2D(Collision2D col)
     {
+        //take damage if the object is a bullet, change the health bar
         if (col.gameObject.tag == "Bullet")
         {
             health -= damage;
--- MH-Prototype/Assets/Scripts/Enemy_Script.cs
+++ MH-Prototype/Assets/Scripts/Enemy_Script.cs
@@ -4,60 +4,68 @@ using UnityEngine;
 
 public class Enemy_Script : MonoBehaviour
 {
+    //things i need to fetch
     public Rigidbody2D rb;
+    public Vector2 ally;
+    public PC_Script pc;
+    public bool bound;
+    
+    //internal variables
     public int health = 10;
     public int lifeLoss = 4;
+    public float ratio = 1.1f;
+    
+    //variables for shooting
     public float timer;
     public float reset = 5f;
-    public Vector2 ally;
     public float delay = 2f;
-    public float ratio = 1.1f;
-    public bool bound;
-    public PC_Script pc;
 
     void Start()
     {
+        //get objects for easy access
         rb = this.GetComponent<Rigidbody2D>();
+        pc = GameObject.FindWithTag("Player").GetComponent<PC_Script>();
+
+        //set up the timer
         timer = reset + Random.Range(-delay, delay);
         reset = timer;
-        pc = GameObject.FindWithTag("Player").GetComponent<PC_Script>();
+        
     }
-
 
     void Update()
     {
+        //timer for enemy to shoot
         timer -= Time.deltaTime;
-
-        if (timer <= 0)
-        {
-            Shoot();
-            timer = reset;
-        }
+        if (timer <= 0) { Shoot(); }
 
         //change velocity here
         Vector3 velocity = rb.velocity;
-
-        if (velocity != new Vector3(0, 0, 0)) 
-        {
-            //CheckSpeed();
+        if (velocity != new Vector3(0, 0, 0)) { //CheckSpeed(); 
         }
 
+        //kill enemy here
         if (health <= 0) { Destroy(gameObject); }
     }
 
-    private void Shoot()
+    public void Shoot()
     {
-        GameObject rBullet = (GameObject)Instantiate(Resources.Load("Bullet"));
+        //reset the timer
+        timer = reset;
 
+        //create the new bullet
+        GameObject rBullet = (GameObject)Instantiate(Resources.Load("Bullet"));
         rBullet.transform.position = new Vector3(transform.position.x, transform.position.y, -2);
 
+        //get the ally location
         ally = GameObject.FindWithTag("Ally").transform.position;
 
+        //call the function in the bullet script
         rBullet.GetComponent<Bullet_Script>().Target(ally);
     }
 
     void OnCollisionEnter2D(Collision2D col)
     {
+        //take damage if the ally runs into the enemy and it is being pulled
         if(col.gameObject.tag == "Ally")
         {
             bound = pc.bound;
@@ -68,6 +76,7 @@ public class Enemy_Script : MonoBehaviour
  
     public void CheckSpeed()
     {
+        //trying to add friction here
         Vector3 velocity = rb.velocity;
 
         if (velocity != new Vector3(0, 0, 0))
